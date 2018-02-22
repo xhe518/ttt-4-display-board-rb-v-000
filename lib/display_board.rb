14ed380bddcad9521ef9d8b637007a6882f0e492
Sid He <xhe518@gmail.com>
2018-02-22 20:52:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,12 @@
 # out the current state.
 
 def display_board(board)
-  board = [" "," "," "," "," "," "," "," "," "]
   puts " #{board(0)} | #{board(1)} | #{board(2)} "
   puts "-----------"
   puts " #{board(3)} | #{board(4)} | #{board(5)} "
   puts "-----------"
   puts " #{board(6)} | #{board(7)} | #{board(8)} "
 end
+
+  board = [" "," "," "," "," "," "," "," "," "]
+  
